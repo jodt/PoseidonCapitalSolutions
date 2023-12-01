9d2d7bda2360d135032643eabc8a7bd0edbb5d7f
joel Dumortier <joel.dumortier@gmail.com>
2023-12-01 14:43:56 +0100

    change strategy to generate id for users

--- src/main/java/com/nnk/springboot/domain/User.java
+++ src/main/java/com/nnk/springboot/domain/User.java
@@ -8,7 +8,7 @@ import jakarta.validation.constraints.NotBlank;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @NotBlank(message = "Username is mandatory")
