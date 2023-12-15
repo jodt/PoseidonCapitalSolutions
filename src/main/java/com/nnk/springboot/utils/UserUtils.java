f1969ca4144977b64f2f8da0ed5341f48fc8896f
joel Dumortier <joel.dumortier@gmail.com>
2023-12-15 15:51:00 +0100

    implement security configuration

---
+++ src/main/java/com/nnk/springboot/utils/UserUtils.java
@@ -0,0 +1,15 @@
+package com.nnk.springboot.utils;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.util.CollectionUtils;
+
+public class UserUtils {
+
+    public static Boolean isAdmin(Authentication authentication){
+
+        return !CollectionUtils.isEmpty(authentication.getAuthorities())
+                && authentication.getAuthorities().contains(new SimpleGrantedAuthority("ADMIN"));
+    }
+
+}
