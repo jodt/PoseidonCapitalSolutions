d803217aade3bcaafd0f935e753915cdb9bf3d55
joel Dumortier <joel.dumortier@gmail.com>
2024-01-05 10:56:18 +0100

    update customSuccessHandler class

--- src/main/java/com/nnk/springboot/security/CustomSuccessHandler.java
+++ src/main/java/com/nnk/springboot/security/CustomSuccessHandler.java
@@ -5,24 +5,22 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 
 import java.io.IOException;
 
 @Slf4j
-public class CustomSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
 
         if (UserUtils.isAdmin(authentication)) {
             String targetUrl = "/user/list";
-            clearAuthenticationAttributes(request);
             getRedirectStrategy().sendRedirect(request, response, targetUrl);
 
         } else {
             String targetUrl = "/bidList/list";
-            clearAuthenticationAttributes(request);
             getRedirectStrategy().sendRedirect(request, response, targetUrl);
         }
 
